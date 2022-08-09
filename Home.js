25fab2b389f55ed21f8c3bad6b597ddac2a94759
Vikash7320 <90022707+Vikash7320@users.noreply.github.com>
2022-08-09 14:28:15 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <>
+      <div className="container-fluid check">
+        <div className="row" >
+          <div className="col-md-12 col-lg-12 fs-1 text-danger">VIKASH KUMAR</div>
+          <div className="col-sm-3 col-md-6 col-lg-2 "></div>
+          <div className="col-md-4  fs-1 text-danger">SOFTWARE DEVELOPER</div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default Home
